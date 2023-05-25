cdba9c4a64915c82b9f60a2bedb26bcc2173ddc5
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-05-25 22:53:06 +0900

    form

--- CWindowsFormsApp/Form1.Designer.cs
+++ CWindowsFormsApp/Form1.Designer.cs
@@ -30,29 +30,66 @@
         {
             this.textBoxName = new System.Windows.Forms.TextBox();
             this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBoxName
             // 
-            this.textBoxName.Location = new System.Drawing.Point(366, 83);
+            this.textBoxName.Location = new System.Drawing.Point(283, 57);
             this.textBoxName.Multiline = true;
             this.textBoxName.Name = "textBoxName";
-            this.textBoxName.Size = new System.Drawing.Size(381, 67);
+            this.textBoxName.Size = new System.Drawing.Size(450, 67);
             this.textBoxName.TabIndex = 0;
             // 
             // textBoxEmail
             // 
-            this.textBoxEmail.Location = new System.Drawing.Point(366, 227);
+            this.textBoxEmail.Location = new System.Drawing.Point(283, 201);
             this.textBoxEmail.Multiline = true;
             this.textBoxEmail.Name = "textBoxEmail";
-            this.textBoxEmail.Size = new System.Drawing.Size(381, 67);
+            this.textBoxEmail.Size = new System.Drawing.Size(450, 67);
             this.textBoxEmail.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("HG丸ｺﾞｼｯｸM-PRO", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(54, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 67);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("HG丸ｺﾞｼｯｸM-PRO", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(54, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 67);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "E-mail";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("HG丸ｺﾞｼｯｸM-PRO", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(481, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(252, 81);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Click!!";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxEmail);
             this.Controls.Add(this.textBoxName);
             this.Name = "Form1";
@@ -66,6 +103,9 @@
 
         private System.Windows.Forms.TextBox textBoxName;
         private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- CWindowsFormsApp/Form1.cs
+++ CWindowsFormsApp/Form1.cs
@@ -16,5 +16,10 @@ namespace CWindowsFormsApp
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
