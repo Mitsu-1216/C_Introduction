79f0a6eb24b63c6769aded768926811500154d27
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-11-07 23:39:25 +0900

    タイトル追加

--- CWindowsFormsApp/Form1.Designer.cs
+++ CWindowsFormsApp/Form1.Designer.cs
@@ -41,10 +41,11 @@
             // 
             this.textBoxEmail.Font = new System.Drawing.Font("HG丸ｺﾞｼｯｸM-PRO", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.textBoxEmail.ForeColor = System.Drawing.SystemColors.InactiveCaption;
-            this.textBoxEmail.Location = new System.Drawing.Point(274, 216);
+            this.textBoxEmail.Location = new System.Drawing.Point(164, 144);
+            this.textBoxEmail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxEmail.Multiline = true;
             this.textBoxEmail.Name = "textBoxEmail";
-            this.textBoxEmail.Size = new System.Drawing.Size(450, 41);
+            this.textBoxEmail.Size = new System.Drawing.Size(272, 29);
             this.textBoxEmail.TabIndex = 1;
             this.textBoxEmail.Text = "aiueo@example.com";
             this.textBoxEmail.TextChanged += new System.EventHandler(this.textBoxEmail_TextChanged_1);
@@ -54,9 +55,10 @@
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("HG丸ｺﾞｼｯｸM-PRO", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(54, 57);
+            this.label1.Location = new System.Drawing.Point(32, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(154, 67);
+            this.label1.Size = new System.Drawing.Size(92, 45);
             this.label1.TabIndex = 2;
             this.label1.Text = "name";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -65,9 +67,10 @@
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("HG丸ｺﾞｼｯｸM-PRO", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label2.Location = new System.Drawing.Point(54, 201);
+            this.label2.Location = new System.Drawing.Point(32, 134);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(154, 67);
+            this.label2.Size = new System.Drawing.Size(92, 45);
             this.label2.TabIndex = 3;
             this.label2.Text = "E-mail";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -75,9 +78,10 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("HG丸ｺﾞｼｯｸM-PRO", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button1.Location = new System.Drawing.Point(481, 333);
+            this.button1.Location = new System.Drawing.Point(289, 222);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(252, 81);
+            this.button1.Size = new System.Drawing.Size(151, 54);
             this.button1.TabIndex = 4;
             this.button1.Text = "Click!!";
             this.button1.UseVisualStyleBackColor = true;
@@ -93,10 +97,11 @@
             // 
             this.textBoxName.Font = new System.Drawing.Font("HG丸ｺﾞｼｯｸM-PRO", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.textBoxName.ForeColor = System.Drawing.SystemColors.InactiveCaption;
-            this.textBoxName.Location = new System.Drawing.Point(274, 72);
+            this.textBoxName.Location = new System.Drawing.Point(164, 48);
+            this.textBoxName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxName.Multiline = true;
             this.textBoxName.Name = "textBoxName";
-            this.textBoxName.Size = new System.Drawing.Size(450, 41);
+            this.textBoxName.Size = new System.Drawing.Size(272, 29);
             this.textBoxName.TabIndex = 6;
             this.textBoxName.Text = "name";
             this.textBoxName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
@@ -105,16 +110,17 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(480, 300);
             this.Controls.Add(this.textBoxName);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxEmail);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "ようこそC#へ！";
             this.ResumeLayout(false);
             this.PerformLayout();
 
